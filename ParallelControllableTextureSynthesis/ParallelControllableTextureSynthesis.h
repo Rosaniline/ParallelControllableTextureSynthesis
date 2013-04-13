a7d38378e5f0255394231a9768824c7645689528
Rosaniline <xup6qup3@gmail.com>
2013-04-13 21:40:33 +0800

    basic framework construction
    done :
      upsampling
      jitter
      correction ( with coherence search )
    
    undergo :
      similar set comparing

--- ParallelControllableTextureSynthesis/ParallelControllableTextureSynthesis.h
+++ ParallelControllableTextureSynthesis/ParallelControllableTextureSynthesis.h
@@ -10,15 +10,54 @@
 #define __ParallelControllableTextureSynthesis__ParallelControllableTextureSynthesis__
 
 #include <iostream>
+#include <fstream>
 #include "RosaniTools.h"
+
+using namespace std;
+using namespace cv;
 
 class ParallelControllableTextureSynthesis {
     
 public:
     
     ParallelControllableTextureSynthesis();
+    ~ParallelControllableTextureSynthesis();
+    
+    cv::Mat synthesis (const string& texture_file, double magnify_ratio);
+    
+
+    
     
 private:
+    
+    void initialization (double magnify_ratio);
+    
+    void upsample (int level);
+    void jitter (int level);
+    void correction (int level);
+    
+    void coordinateMapping (int level);
+    void coordinateTrim (Point& coor);
+    
+    void similarSetConstruction ();
+    
+    Mat sample_texture;
+    Mat synthesized_texture;
+    vector<dynamicArray2D<Point> > syn_coor;
+    vector<Mat> syn_texture;
+    dynamicArray2D<vector<Point> > sample_similar_set;
+    
+    string sample_texture_path;
+    
+    static const int    PYRAMID_LEVEL       =   2;
+    static const int    OUTSPACE_FACTOR     =   1;
+    static const int    JITTER_AMPLITUDE    =   1;
+    static const int    PATCH_WIDTH         =   2;
+    static const int    COHERENCE_SEARCH_W  =   5;
+    static const int    SIMILAR_NEIGHBOR_N  =  10;
+    
+    
+    
     
 };
 
--- ParallelControllableTextureSynthesis/main.cpp
+++ ParallelControllableTextureSynthesis/main.cpp
@@ -7,13 +7,48 @@
 //
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "ParallelControllableTextureSynthesis.h"
 
 
 int main(int argc, const char * argv[])
 {
+    ParallelControllableTextureSynthesis textsyn = ParallelControllableTextureSynthesis();
     
     
+    textsyn.synthesis("/Users/xup6qup3/Google Drive/code/Dev.temp/ParallelControllableTextureSynthesis/ParallelControllableTextureSynthesis/tx.jpg", 4.0);
+    
+//    ifstream ifile("/Users/xup6qup3/Google Drive/code/Dev.temp/ParallelControllableTextureSynthesis/ParallelControllableTextureSynthesis/tx.txt");
+//    
+//    if ( !ifile.eof() ) {
+//        
+////        int i = 0;
+//        string s;
+//        getline(ifile, s);
+//        cout<<s<<endl;
+//        
+//        while ( !ifile.eof() ) {
+//            
+//            int i = 0, j = 0, nx = 0, ny = 0;
+//            ifile >> i; ifile >> j;
+//            
+//            cout<<"("<<i<<", "<<j<<")  ";
+//            
+//            for (int i = 0; i < 10; i ++) {
+//                
+//                ifile >> nx; ifile >> ny;
+//                
+//                cout<<nx<<", "<<ny<<" : ";
+//                
+//            }
+//            
+//            cout<<endl;
+//        }
+//        
+//    }
+
+    
+
+
 
     return 0;
 }
